96da103c1abb284f723a492c92e15b7b5ad865b0
shaon82 <mshaon151@gmail.com>
2024-05-30 00:40:58 +0600

    first commit

---
+++ src/Rectangle.java
@@ -0,0 +1,6 @@
+public class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("this is draw method inside Rectangle.");
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,6 @@
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("this is draw method inside Square.");
+    }
+}
